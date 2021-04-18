549f6262bc58580e0d94aea239d2303d59618f1c
Nitin Raj <nitinraj2001@gmail.com>
2021-04-18 10:17:55 +0530

    This is my first commit using angular practice

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson} from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+ theSalesPersonList : SalesPerson[]=[new SalesPerson("nitin","raj","nitinraj567@gmail.com",5678),
+                                     new SalesPerson("ashish","kumar","ashishkumar2001@gmail.com",8988),
+                                     new SalesPerson("ashutosh","pathak","ashutoshpathak2987@gmail.com",5696),
+                                     new SalesPerson("puspak","sharma","puspak5467@gmail.com",9578)];
+
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
